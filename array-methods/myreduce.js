2afe283cac423415032faeb8a2a690e9877c7523
artempoo <77047966+artempoo@users.noreply.github.com>
2025-09-04 15:28:18 +0300

    исправил ошибки

--- array-methods/myreduce.js
+++ array-methods/myreduce.js
@@ -1,19 +1,31 @@
 Array.prototype.myReduce = function(callback, initialValue) {
-	const originalArr = this;
+	//Проверка, что callback - функция
+	if (typeof callback !== "function") {
+		throw new TypeError("callback must be a function");
+	}
+	const arr = this;
+
+	if (arr == null) {
+		throw new TypeError("Array is null or not defined");
+	}
+
+	const len = arr.length >>> 0;
 
 	let acc, startIndex;
 
 	if (initialValue === undefined) {
-		if (originalArr.length === 0) throw new TypeError("Array is empty");
-		acc = originalArr[0];
+		if (len === 0) throw new TypeError("Array is empty");
+		acc = arr[0];
 		startIndex = 1;
 	} else {
 		acc = initialValue;
 		startIndex = 0;
 	}
 
-	for (let i = startIndex; i < originalArr.length; i++) {
-		acc = callback.call(thisArg, originalArr[i], i, originalArr);
+	for (let i = startIndex; i < len; i++) {
+		if (i in arr) {
+			acc = callback(acc, arr[i], i, arr);
+		}
 	}
 
 	return acc;
